39848cd80f6d8a42db51002d245614a5ec4af37e
Fritz Marvin Neumann <marvin.neumann@gmail.com>
2019-02-22 01:08:15 +0100

    fixed vhost conf

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-Docker LAMP
+Docker containers:
+
+ - PHP 5.6 + Apache 2.4
+ - MySQL 5.7
+
+ Start containers with `docker-compose up` and remove with `docker-compose down`
--- src/index.php
+++ src/index.php
@@ -1,13 +1,2 @@
 <?php
-$user = "devdb_dev";
-$pass = "devdb1234";
-try {
-    $dbh = new PDO('mysql:host=mysql;dbname=devdb', $user, $pass);
-    foreach($dbh->query('SELECT * from FOO') as $row) {
-        var_dump($row);
-    }
-    $dbh = null;
-} catch (PDOException $e) {
-    print "Error!: " . $e->getMessage() . "<br/>";
-    die();
-}
+phpinfo();
